374543ee73e37cc056888a0acc042d6243195e71
501A <art71903design@gmail.com>
2025-02-27 01:15:28 -0800

    Refactor `Profile` styling

--- app/[userId]/Profile.tsx
+++ app/[userId]/Profile.tsx
@@ -23,20 +23,11 @@ export default function Profile({ user }: { user: User }) {
       <PopoverContent
         align="end"
         sideOffset={4}
-        className="bg-white border border-zinc-200 p-4 rounded-2xl shadow-md min-w-60 flex flex-col gap-4"
+        className="bg-white border border-zinc-200 p-3 rounded-2xl shadow-md min-w-60 flex flex-col gap-6"
       >
-        <div className="flex items-center gap-3">
-          <Image
-            src={user.photoURL || ""}
-            alt="Profile Picture"
-            width={32}
-            height={32}
-            className="rounded-full"
-          />
-          <div>
-            <h6>{user.displayName}</h6>
-            <div className="text-sm text-zinc-500">{user.email}</div>
-          </div>
+        <div className="px-3 pt-2">
+          <h6>{user.displayName}</h6>
+          <div className="text-sm text-zinc-500">{user.email}</div>
         </div>
         <button
           onClick={() => {
