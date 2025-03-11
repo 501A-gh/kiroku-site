c675efc099bc97556f631c654b88de475c99e48b
Zachariah-W <zachwangs@gmail.com>
2025-03-10 23:07:45 -0700

    Added contact in dialog #9

--- app/[userId]/Support.tsx
+++ app/[userId]/Support.tsx
@@ -5,7 +5,7 @@ import {
   DialogTitle,
 } from "@/components/Dialog";
 import { DialogTrigger } from "@radix-ui/react-dialog";
-import { HelpCircle } from "react-feather";
+import { HelpCircle, Link2 } from "react-feather";
 
 const Support = () => {
   return (
@@ -15,26 +15,38 @@ const Support = () => {
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>How To Use</DialogTitle>
+          <DialogTitle>Supprort and Contact</DialogTitle>
         </DialogHeader>
-        <div className="px-4 pb-4">
-          <b>Adding an Item</b>
-          <ul>
-            <li>Enter the product name and (optional) description.</li>
-            <li>Select a category from the dropdown menu.</li>
-            <li>Pick the purchase and expiration dates.</li>
-            <li>
-              Click the <strong>“+ Add Item”</strong> button to save it.
-            </li>
-          </ul>
-          <br />
-          <b>Managing Items</b>
-          <ul>
-            <li>View all added items under the "All Items" section.</li>
-            <li>
-              Use the filter options to sort by category or purchase date.
-            </li>
-          </ul>
+        <div>
+          <div className="px-4 pb-4 flex flex-col gap-3">
+            <b>Adding an Item</b>
+            <ul>
+              <li>Enter the product name and (optional) description.</li>
+              <li>Select a category from the dropdown menu.</li>
+              <li>Pick the purchase and expiration dates.</li>
+              <li>
+                Click the <strong>“+ Add Item”</strong> button to save it.
+              </li>
+            </ul>
+            <b>Managing Items</b>
+            <ul>
+              <li>View all added items under the "All Items" section.</li>
+              <li>
+                Use the filter options to sort by category or purchase date.
+              </li>
+            </ul>
+            <div className="pr-5 flex flex-col gap-1">
+              <b>Contact Us</b>
+              <div className="w-full border border-zinc-200 bg-zinc-50 rounded-lg p-2">
+                <a
+                  className="text-zinc-800 no-underline hover:text-blue-600"
+                  href=" https://forms.gle/UTZRv5P7R9jwsXvG7"
+                >
+                  https://forms.gle/UTZRv5P7R9jwsXvG7
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </DialogContent>
     </Dialog>
