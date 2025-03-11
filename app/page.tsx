e2625a50939b1f7e11537c9cb135b085e702511e
501A <art71903design@gmail.com>
2025-03-11 15:54:24 -0700

    Add new logo with updated LP layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-8 items-center justify-center p-24 min-h-screen">
-      <div className="flex flex-row items-center">
+    <main className="flex flex-col gap-12 items-center justify-center p-24 min-h-screen">
+      <div className="flex flex-col items-center gap-8">
         <Image
-          src="/Kiroku-Logo.png"
+          src="/logo.png"
           alt="Kiroku Logo"
-          width={120}
-          height={120}
+          className="bg-radial-[at_50%_35%] from-white from-55% to-emerald-100 rounded-3xl border border-zinc-200 border-b-2 drop-shadow-xs"
+          width={100}
+          height={100}
+          quality={100}
         />
-        <hgroup className="flex items-start flex-col gap-2">
+        <hgroup className="flex items-center flex-col gap-2">
           <h1>Kiroku</h1>
-          <h4 className="text-zinc-500">
-            An app to help
-            <br /> organize your fridge
-          </h4>
+          <h4 className="text-zinc-500">An app to help organize your fridge</h4>
         </hgroup>
       </div>
       <SignInWithGoogle />
