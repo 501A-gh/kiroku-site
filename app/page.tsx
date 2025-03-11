0f95d7ef317506f315b384c883dd2bc4f0445311
501A <art71903design@gmail.com>
2025-03-11 16:04:51 -0700

    Remove logo styling

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex flex-col gap-12 items-center justify-center p-24 min-h-screen">
-      <div className="flex flex-col items-center gap-8">
+      <div className="flex flex-col items-center gap-4">
         <Image
           src="/logo.png"
           alt="Kiroku Logo"
-          className="bg-radial-[at_50%_35%] from-white from-55% to-emerald-100 rounded-3xl border border-zinc-200 border-b-2 drop-shadow-xs"
           width={100}
           height={100}
           quality={100}
